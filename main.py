6ce9fe0127e0367595a7a26681566bf1ae18bc7b
Colin <leocolin7002@gmail.com>
2025-10-11 21:07:49 +0200

    marginal fixes

--- main.py
+++ main.py
@@ -20,17 +20,18 @@ if loc:
 else:
     print("⚠️ Impossible d'appliquer la locale française — affichage des dates en ISO.")
 
-
-# --- Création de l'application FastAPI ---
+#--- Initialisation de l'application FastAPI ---
 app = FastAPI(
     title="Monday Briefing API",
-    description="API pour la génération automatique du briefing hebdomadaire (météo, PSG, ville aléatoire).",
+    description="""API pour la génération automatique et l'envoi par mail d'un briefing hebdomadaire procurant : 
+    la météo autour d'une localisation, les jours optimaux de la semaine pour aller courir, 
+    les deux prochains matchs de foot de l'équipe choisie, une ville française aléatoire et sa population.""",
     version="2.1.0",
     lifespan=lifespan
 )
 
 
-# --- Montage des routes ---
+# Routes
 app.include_router(endpoints_briefing.router)
 app.include_router(endpoints_config.router)
 app.include_router(endpoints_health.router)
